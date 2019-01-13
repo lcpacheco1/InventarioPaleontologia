160d74655c18764e199f9003976f6f10ce8b9742
Carmenjara <chiojaramillo2@gmail.com>
2019-01-13 13:44:30 -0500

    archivo index.html login

---
+++ SistemaInventarioPaleontologia/js/main.js
@@ -0,0 +1,155 @@
+jQuery(document).ready(function($){
+	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+	var MqL = 1170;
+	//move nav element position according to window width
+	moveNavigation();
+	$(window).on('resize', function(){
+		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+	});
+
+	//mobile - open lateral menu clicking on the menu icon
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		if( $('.cd-main-content').hasClass('nav-is-visible') ) {
+			closeNav();
+			$('.cd-overlay').removeClass('is-visible');
+		} else {
+			$(this).addClass('nav-is-visible');
+			$('.seccion-buscar-boton').addClass('nav-is-visible');
+			$('.cd-main-content').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('body').addClass('overflow-hidden');
+			});
+			toggleSearch('close');
+			$('.cd-overlay').addClass('is-visible');
+		}
+	});
+
+	//open search form
+	$('.cd-search-trigger').on('click', function(event){
+		event.preventDefault();
+		toggleSearch();
+		closeNav();
+	});
+
+	function Presionar(){
+		alert (" EXITO");
+		document.getElementById('login').click();
+	}
+	
+	$('.btnLogin').on('click', function(event){
+		$('.principal').addClass('opacity-body');
+	});
+
+	$('.btnComent1').on('click', function(event){
+		$('.listaComnt1').addClass('display');
+	});
+
+	$('.btnComent2').on('click', function(event){
+		$('.listaComnt2').addClass('display');
+	});
+	
+	$('.btnCerrar').on('click', function(event){
+		$('.principal').removeClass('opacity-body');
+	});
+
+	$('.btnRegistrar').on('click', function(event){
+		$('.login').addClass('principal');
+		$('.registro').removeClass('principal');
+		$('.principal2').addClass('visible');
+	});
+
+	$('.btnVlogueo').on('click', function(event){
+		$('.registro').addClass('principal');
+		$('.login').removeClass('principal');
+		$('.principal2').removeClass('visible');
+	});
+
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#header-main').addClass('fixed');
+		} else {
+			$('#header-main').removeClass('fixed');
+		}
+	});
+
+
+	//submenu items - go back link
+	$('.go-back').on('click', function(){
+		$(this).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('moves-out');
+	});
+
+	function closeNav() {
+		$('.cd-nav-trigger').removeClass('nav-is-visible');
+		$('.seccion-buscar-boton').removeClass('nav-is-visible');
+		$('.cd-primary-nav').removeClass('nav-is-visible');
+		$('.has-children ul').addClass('is-hidden');
+		$('.has-children a').removeClass('selected');
+		$('.moves-out').removeClass('moves-out');
+		$('.cd-main-content').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			$('body').removeClass('overflow-hidden');
+		});
+	}
+
+	function toggleSearch(type) {
+		if(type=="close") {
+			//close serach 
+			$('.cd-search').removeClass('is-visible');
+			$('.cd-search-trigger').removeClass('search-is-visible');
+			$('.cd-overlay').removeClass('search-is-visible');
+		} else {
+			//toggle search visibility
+			$('.cd-search').toggleClass('is-visible');
+			$('.cd-search-trigger').toggleClass('search-is-visible');
+			$('.cd-overlay').toggleClass('search-is-visible');
+			if($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
+			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible') ;
+		}
+	}
+
+	function checkWindowWidth() {
+		//check window width (scrollbar included)
+		var e = window, 
+            a = 'inner';
+        if (!('innerWidth' in window )) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        if ( e[ a+'Width' ] >= MqL ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function moveNavigation(){
+		var navigation = $('.cd-nav');
+  		var desktop = checkWindowWidth();
+        if ( desktop ) {
+			navigation.detach();
+			navigation.insertBefore('.boton-buscar');
+		} else {
+			navigation.detach();
+			navigation.insertAfter('.cd-main-content');
+		}
+	}
+
+	$('.arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 1000);
+	});
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 400 ){
+			$('.arriba').slideDown(300);
+		}else{
+			$('.arriba').slideUp(300);
+		}
+	});
+});
+function Presionar(){  	//document.write("er");
+	alert (" EXITO");
+	document.getElementById('login').click();
+}
