df8d501db5a79f76137ac27724921e177ce20493
Carmenjara <chiojaramillo2@gmail.com>
2019-01-23 16:43:41 -0500

    Desplegable registro Ficha

---
+++ New/SistemaInventarioPaleontologia/dll/seccionesNav.php
@@ -0,0 +1,294 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+
+    <title>Contactenos</title>
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+   <link href=dashboard/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="dashboard/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Page level plugin CSS-->
+    <link href="dashboard/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="dashboard/css/sb-admin.css" rel="stylesheet">
+  
+  </head>
+  <body id="page-top">
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark  navegacion" >
+        
+          <a class="navbar-brand img_menu" href="../index.html"><img src="../imagenes/logo/logo_mapa.png"></a>
+         <a class="navbar-brand img_menu_mae" href="../index.html"><img src="../imagenes/logo/logo_plat.png"></a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+        <div class="collapse navbar-collapse mr-auto justify-content-end" id="navbarSupportedContent">
+          <ul class="navbar-nav menu ">
+            <li class="nav-item li">
+             <a class="nav-link" href="../index.html">Inicio </a>
+            </li>  
+            <li class="nav-item active">
+              <li class="nav-item dropdown li">
+                <a class="nav-link dropdown-toggle menu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Tematicas
+                </a>
+                <div class="dropdown-menu submenu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#/Investigaciones.html"">
+                    <img src="../imagenes/inv_menu.png">
+                    <h6>Investigaciones</h6> 
+                  </a>
+                  <a class="dropdown-item" href=CentrosT/CentrosT.html">
+                    <img src="../imagenes/centros_t_menu.png">
+                    <h6>Centros de Tenencia </h6> 
+                  </a>
+                  <a class="dropdown-item" href="AreasC.html">
+                    <img src="../imagenes/areas_menu.png">
+                    <h6>Areas de Conservación </h6>              
+                  </a>
+                  <a class="dropdown-item" href="Xiloteca.html">
+                    <img src="../imagenes/xil_menu.png">
+                    <h6>Xiloteca </h6> 
+                  </a>
+                  <a class="dropdown-item" href="Proyectos.html">
+                    <img src="../imagenes/proyectos_menu.png">
+                    <h6> Proyectos</h6>
+                  </a>
+              </div>
+              </li>
+              </li>
+            <li class="nav-item li">
+              <a class="nav-link" href="acercae.html">Acerca de </a>
+            </li> 
+             <li class="nav-item li">
+             <a class="nav-link" href="#">Contactos </a>
+            </li>  
+          </ul>    
+        </div>
+      </nav>
+    </header>
+        <!-- Parte intermedia de la página(direccion) --><!-- -->
+        <main class="contMain container">
+          <ul class="nav nav-tabs justify-content-center m-4" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Direccion provincial de Loja</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Direccion provincial de El Oro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Direccion provincial de Zamora Chinchipe</a>
+            </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="map-responsive">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d509220.7633271628!2d-79.2541924057374!3d-4.357137057326698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91cb1fb2e66c6153%3A0xf06fa3a6d57268be!2sParque+Nacional+Podocarpus!5e0!3m2!1ses-419!2sec!4v1540771030783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-md-5">
+                  <h5 class="text-center my-4">Dirección Provincial del Ambiente de Loja</h5>
+                  <p class="text-center my-0">Dirección: Sevilla de Oro y Francisco de Orellana esquina, frente al hospital</p>
+                  <p class="text-center my-0">Teléfonos: 07 2 605315  / 07 2606606</p>
+                  <p class="text-center my-0">Correo electrónico: elsy.silva@gmail.com</p>
+                </div>
+              </div>
+              <h3 class="text-center m-4">OFICINAS TÉCNICAS</h3>
+              
+              <hr>
+              <div class="row">   
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina técnica de Loja</h5>
+                  <p class="my-0 text-center">Dirección: Loja, calle Sucre entre Imbabura y Quito</p>
+                  <p class="my-0 text-center">Teléfonos: 07 2571534 / 07 2 577-125 ext 107</p>
+                  <p class="my-0 text-center">Correo electrónico: santos.calderon@ambiente.gob.ec</p>
+                  <p class="my-0 text-center">Jurisdicción: Loja, Catamayo, Saraguro, Olmedo, Chaguarpamba y Paltas</p>
+                </div>
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina técnica Gonzanamá</h5>
+                  <p class="my-0 text-center">Dirección: Gonzanamá, calle Loja entre Guillermo Bravo y Bolívar</p>
+                  <p class="my-0 text-center">Teléfono: 07 22664398</p>
+                  <p class="my-0 text-center">Correo electrónico: cesar.figueroa@ambiente.gob.ec</p>
+                  <p class="my-0 text-center">Jurisdicción: Calvas, Espíndola, Gonzanamá y Quilanga</p>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica Alamor</h5>
+                  <p class="my-0 text-center">Dirección: Alamor, calle Simón Bolívar y Juan Montalvo</p>
+                  <p class="my-0 text-center">Teléfonos: 07 2680409</p>
+                  <p class="my-0 text-center">Correo electrónico: juan.barrazueta@ambiente.gob.ec</p>
+                  <p class="my-0 text-center">Jurisdicción: Celica, Pindal y Puyango</p>
+                </div>
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica Zapotillo</h5>
+                  <p class="my-0 text-center">Dirección: Zapotillo,  Avenida Amazonas y Hno. Miguel</p>
+                  <p class="my-0 text-center">Teléfonos: 07 2647107</p>
+                  <p class="my-0 text-center">Correo electrónico: cesar.caraguay@ambiente.gob.ec</p>
+                  <p class="my-0 text-center">Correo electrónico: cesar.caraguay@ambiente.gob.ec</p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="map-responsive">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d509220.7633271628!2d-79.2541924057374!3d-4.357137057326698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91cb1fb2e66c6153%3A0xf06fa3a6d57268be!2sParque+Nacional+Podocarpus!5e0!3m2!1ses-419!2sec!4v1540771030783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-md-5">
+                  <h5 class="text-center my-2">Dirección Provincial del Ambiente El Oro</h5>
+                  <p>Dirección: Vela entre 25 de Junio y Sucre quinto piso alto del Edificio Ex PREDESUR</p>
+                  <p>Teléfonos: 07 2934596 / 07 2961965</p>
+                  <p>Correo electrónico: geomar.loaiza@ambiente.gob.ec</p>
+                  <p>Twitter: @MAEELOro</p>
+                </div>
+              </div>
+              <h3 class="text-center m-4">OFICINAS TÉCNICAS</h3>
+              
+              <hr>
+              <div class="row">   
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica El Oro</h5>
+                  <p class="text-center my-0">Dirección: Vela entre 25 de Junio y Sucre quinto piso alto del Edificio Ex PREDESUR</p>
+                  <p class="text-center my-0">Teléfonos: 07 2934596 / 07 2961965 ext 15</p>
+                  <p class="text-center my-0">Correo electrónico: manuel.tinedo@ambiente.gob.ec</p>
+                  <p class="text-center my-0">Jurisdicción: Arenillas, Chilla, El Guabo, Huaquillas, Las Lajas, Machala,  Pasaje y Santa Rosa.</p>
+                </div>
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica Zaruma</h5>
+                  <p class="text-center my-0">Dirección: Barrio Sexmo, calle Damian Meneces. Edficio Ex Motel Munucipal.</p>
+                  <p class="text-center my-0">Teléfonos: 07 2973765</p>
+                  <p class="text-center my-0">Correo electrónico: lida.leon@ambiente.gob.ec</p>
+                  <p class="text-center my-0">Jurisdicción: Atahualpa, Balsas, Marcabeli, Piñas, Portovelo y Zaruma.</p>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+              <div class="row">
+                <div class="col-md-7">
+                  <div class="map-responsive">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d509220.7633271628!2d-79.2541924057374!3d-4.357137057326698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91cb1fb2e66c6153%3A0xf06fa3a6d57268be!2sParque+Nacional+Podocarpus!5e0!3m2!1ses-419!2sec!4v1540771030783" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-md-5">
+                  <h5 class="text-center my-2">Dirección Provincial del Ambiente de Zamora Chinchipe</h5> 
+                  <p class="my-0 text-center">Dirección: Sevilla de Oro y Francisco de Orellana esquina, frente al hospital</p>
+                  <p class="my-0 text-center">Teléfonos: 07 2 605315  / 07 2606606</p>
+                  <p class="my-0 text-center">Correo electrónico: elsy.silva@gmail.com</p>
+                  <p class="my-0 text-center">Twitter:</p>
+                </div>
+              </div>
+              <h3 class="text-center m-4">OFICINAS TÉCNICAS</h3>
+              
+              <hr>
+              <div class="row">   
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica Zamora</h5>
+                  <p class="text-center my-0">Dirección: Sevilla de Oro y Francisco de Orellana esquina, frente al hospital</p>
+                  <p class="text-center my-0">Teléfonos: 07 2 605315  / 07 2606606 ext 15</p>
+                  <p class="text-center my-0">Correo electrónico: byron.medina@ambiente.gob.ec </p>
+                  <p class="text-center my-0">Jurisdicción: Centinela del Cóndor, Chinchipe, El Pangui, Nangaritza, Palanda, Paquisha, Yacuambi, Yantzaza y Zamora</p>
+                </div>  
+                <div class="col-md-6">
+                  <h5 class="text-center py-2">Oficina Técnica Valladolid</h5>
+                  <p class="text-center my-0">Dirección: Valladolid calle Colonia Agrícola entre Carlos Larreátegui y Av. Amazonas</p>
+                  <p class="text-center my-0">Teléfonos: 07 03041662</p>
+                  <p class="text-center my-0">Correo electrónico: jose.luzuriaga@ambiente.gob.ec </p>
+                  <p class="text-center my-0">Jurisdicción: Palanda y Zumba</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <!-- Parte inferior de la página (footer) -->
+         <footer>
+  <div class="w-100 p-3" >
+  <div class="row">
+    <div class="col-1">
+    </div>
+    <div class="col-2">
+      <div class="row">
+        <div class="col-12">
+        <a href="http://www.ambiente.gob.ec/"  target="_blank">
+          <img class="mw-100" src="../imagenes/logo/mae_logo.png"  alt="Max-width 100%">
+        </a>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-4">
+            <a  href="https://www.todaunavida.gob.ec/"  target="_blank">
+              <img class="mw-100" src="../imagenes/logo/tuv.png" alt="Max-width 100%">
+            </a>
+          </div>
+          <div class="col-5">
+              <img class="mw-100" src="../imagenes/logo/gob_logo.png" alt="Max-width 100%">
+          </div>
+      </div>
+    </div>
+    
+    <div class="col-4">
+    </div>
+    <div class="col-1">
+      <a href="http://suia.ambiente.gob.ec/"  target="_blank">
+        <img class="mw-100" src="../imagenes/logo/suia.png"  alt="Max-width 100%">
+      </a>
+    </div>
+    <div class="col-1" >
+      <a href="http://areasprotegidas.ambiente.gob.ec/">
+          <p id="sis_ar_p">
+             ÁREAS PROTEGIDAS
+          </p>
+      </a>
+    </div>
+    <div class="col-1">
+      <a href="http://www.ecu911.gob.ec/"  target="_blank">
+        <img class="mw-100" src="../imagenes/logo/ecu911.png"  alt="Max-width 100%">
+      </a>
+    </div>
+   <div class="col-sm-1">
+      <img class="mw-100" src="../imagenes/project/1800delito.png"  alt="Max-width 100%">
+    </div>
+  </div>
+  <div class="row"> 
+    <div class="col-sm-1"></div>
+    
+  </div>
+  
+</div>
+<div class="w-100 p-3 " style="background: #556d59">
+  <div class="row">
+    <div class="col-5">
+    </div>
+    <div class="col-2">
+      <h6 style="color: white"> © Copyright 2018 HTML.am</h6>
+    </div>
+    <div class="col-5">
+    </div>
+  </div>
+    
+  </div>
+</footer>
+    
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="../js/Popper.min.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.vide.min.js"></script>
+    <script type="text/javascript" src='https://maps.google.com/maps/api/js?sensor=false&libraries=places&key=API_KEY'></script>
+    <script src="locationpicker.jquery.min.js"></script>
+  </body>
+</html>
---
+++ New/SistemaInventarioPaleontologia/dominio/DataBase.class.php
@@ -0,0 +1,19 @@
+<?php
+function conectaDb()
+{
+    //Conexión con MySQL
+    try {
+        $tmp = new PDO(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD);
+        $tmp->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+        $tmp->exec("set names utf8mb4");
+        return($tmp);
+    } catch(PDOException $e) {
+        cabecera("Error grave", MENU_PRINCIPAL);
+        print "    <p>Error: No puede conectarse con la base de datos.</p>\n";
+        print "\n";
+        print "    <p>Error: " . $e->getMessage() . "</p>\n";
+        pie();
+        exit();
+    }
+}
+?>
